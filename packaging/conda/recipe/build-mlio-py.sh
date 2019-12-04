223b2fea8598e45abf4b06d5b417914bd4f2d304
Can Balioglu <cbalioglu@users.noreply.github.com>
2019-12-04 15:29:25 +0000

    Update cmake target names

--- packaging/conda/recipe/build-mlio-py.sh
+++ packaging/conda/recipe/build-mlio-py.sh
@@ -6,15 +6,16 @@ set -o errexit
 cd "$SRC_DIR/build/mlio"
 
 cmake -DMLIO_INCLUDE_LIB=FALSE\
-      -DMLIO_INCLUDE_PYTHON_EXTENSION=TRUE\
       -DMLIO_INCLUDE_CONTRIB=TRUE\
+      -DMLIO_INCLUDE_PYTHON_EXTENSION=TRUE\
+      -DMLIO_INCLUDE_ARROW_INTEGRATION=TRUE\
       -DPYTHON_EXECUTABLE="$(command -v python)"\
       "$SRC_DIR"
 
 cmake --build . --target clean
-cmake --build . --target mlio-py-core
-cmake --build . --target mlio-py-arrow
-cmake --build . --target mlio-contrib-insights
+cmake --build . --target mlio-py
+cmake --build . --target mlio-arrow
+cmake --build . --target mlio-contrib
 
 # Using the python-config script is not reliable as Conda does not
 # include it in the dependency closure.
